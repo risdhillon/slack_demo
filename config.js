eeb0fc375401d134d3c73c8b49ad4c32bd472260
Risham <t-ridhil@microsoft.com>
2017-07-17 11:52:43 -0700

    added new api token

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
   slackUrl: 'https://pscstest.slack.com',
   // access token of slack
   // You can generate it in https://api.slack.com/web#auth
-  slacktoken: 'xoxp-205317946199-204410186291-205323770199-83b5ae700b2ec442013ddc28764ccdf7',
+  slacktoken: 'xoxp-205317946199-204410186291-215003837159-83c5a7b0eafd30bd3b986a2702bcddbc',
   // an optional security measure - if it is set, then that token will be required to get invited.
   inviteToken: null,
   locale: process.env.LOCALE || "en",
