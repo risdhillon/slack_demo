26e67eef107da8f4f5c98aef4e9bb4169ad25ccd
Risham <t-ridhil@microsoft.com>
2017-07-17 15:35:23 -0700

    edited bg and ps to ps2

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // your community or team name to display on join page.
-  community: process.env.COMMUNITY_NAME || 'PSCS Test',
+  community: process.env.COMMUNITY_NAME || 'PSCS Test 2',
   // your slack team url (ex: socketio.slack.com)
   slackUrl: process.env.SLACK_URL || 'pscstest.slack.com',
   // access token of slack
