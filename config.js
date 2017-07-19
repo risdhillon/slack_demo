d65b148a01c198e332a1af786f7d6130e05ae9e5
Risham <t-ridhil@microsoft.com>
2017-07-19 10:24:47 -0700

    designer changes

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // your community or team name to display on join page.
-  community: process.env.COMMUNITY_NAME || 'YOUR TEAM NAME HERE',
+  community: process.env.COMMUNITY_NAME || 'PS Cloud Channel',
   // your slack team url (ex: socketio.slack.com)
   slackUrl: process.env.SLACK_URL || 'pscstest.slack.com',
   // access token of slack
